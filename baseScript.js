7414bc553f66335245562f2a3d0995dcd19dcaf6
mrbezZ <90276488+mrbezZ@users.noreply.github.com>
2022-04-02 02:19:56 +0300

    Add files via upload

---
+++ baseScript.js
@@ -0,0 +1,10 @@
+
+
+function init(){
+    var rulesButton = document.getElementById("rulesButt")
+    rulesButton.onclick = goRules;
+}
+
+function goRules() {
+    alert("Вы нажали на кнопку");
+}
